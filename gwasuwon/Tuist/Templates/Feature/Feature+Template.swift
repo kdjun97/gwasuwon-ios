852a35ac60018a0d773a7887d08b5970fc567b99
kdjun97 <kdjun97@gmail.com>
2024-05-23 00:21:19 +0900

    feat/tuist-initial-setting: add template for feature and module

---
+++ gwasuwon/Tuist/Templates/Feature/Feature+Template.swift
@@ -0,0 +1,45 @@
+//
+//  Feature+Template.swift
+//  gwasuwonManifests
+//
+//  Created by 김동준 on 5/12/24
+//
+
+import ProjectDescription
+
+private let nameAttribute = Template.Attribute.required("name")
+private let path = "Projects/Features/\(nameAttribute)"
+
+private let projectContents = """
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.module(name: \"\(nameAttribute)\")
+"""
+
+private let template = Template(
+    description: "A template for a new feature module",
+    attributes: [nameAttribute],
+    items: [
+        .string(
+            path: "\(path)/Project.swift",
+            contents: projectContents
+        ),
+        .file(
+            path: "\(path)/Sources/\(nameAttribute)View.swift",
+            templatePath: "./Source/ViewTemplateCode.swift"
+        ),
+        .file(
+            path: "\(path)/Sources/\(nameAttribute)Feature.swift",
+            templatePath: "./Source/FeatureTemplateCode.swift"
+        ),
+        .string(
+            path: "\(path)/Tests/Sources/TestCode.swift",
+            contents: "// Test Source Code Here"
+        ),
+        .file(
+            path: "\(path)/Tests/Support/Info.plist",
+            templatePath: "../Common/InfoPlist.stencil"
+        )
+    ]
+)
