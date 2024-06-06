b79e699cf35b4a28f0ccf1fe75b4280aa129c1b4
kdjun97 <kdjun97@gmail.com>
2024-06-06 15:41:21 +0900

    feat/sign-up-module: add sign up module

--- gwasuwon/Tuist/ProjectDescriptionHelpers/Dependency/DependencyInformation.swift
+++ gwasuwon/Tuist/ProjectDescriptionHelpers/Dependency/DependencyInformation.swift
@@ -9,14 +9,16 @@ import Foundation
 
 let dependencyInfo: [DependencyInformation: [DependencyInformation]] = [
     .App: [.RootFeature],
-    .RootFeature: [.SignInFeature, .DesignSystem, .ComposableArchitecture],
-    .SignInFeature: [.DesignSystem, .ComposableArchitecture]
+    .RootFeature: [.SignInFeature, .SignUpFeature, .DesignSystem, .ComposableArchitecture],
+    .SignInFeature: [.DesignSystem, .ComposableArchitecture],
+    .SignUpFeature: [.DesignSystem, .ComposableArchitecture],
 ]
 
 public enum DependencyInformation: String {
     case App = "App"
     case RootFeature = "RootFeature"
     case SignInFeature = "SignInFeature"
+    case SignUpFeature = "SignUpFeature"
     case ComposableArchitecture = "ComposableArchitecture"
     case DesignSystem = "DesignSystem"
 }
