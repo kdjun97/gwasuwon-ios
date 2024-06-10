bb8831f82ac2dcbda3eeed614392128ad36368ca
kdjun97 <kdjun97@gmail.com>
2024-06-10 20:39:08 +0900

    feat/network: fix access modifier

--- gwasuwon/Projects/Data/Sources/Remote/Extensions/Encodable+Extension.swift
+++ gwasuwon/Projects/Data/Sources/Remote/Extensions/Encodable+Extension.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension Encodable {
-    public func toDictionary() throws -> [String: Any]? {
+    func toDictionary() throws -> [String: Any]? {
         let data = try JSONEncoder().encode(self)
         let jsonObject = try JSONSerialization.jsonObject(with: data)
         return jsonObject as? [String: Any]
