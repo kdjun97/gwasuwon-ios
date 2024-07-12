440daf182f4497a1e80012f7fdcd9fb05ca8ddf3
kdjun97 <kdjun97@gmail.com>
2024-07-13 00:43:50 +0900

    feat/home: AddClass View Done

--- gwasuwon/Projects/App/Sources/Extensions/UIApplication+Extension.swift
+++ gwasuwon/Projects/App/Sources/Extensions/UIApplication+Extension.swift
@@ -20,6 +20,6 @@ extension UIApplication {
 
 extension UIApplication: UIGestureRecognizerDelegate {
     public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-        return true
+        return false
     }
 }
