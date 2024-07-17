eb8ab659f747cc1838e516cc42e656c7ba7dd6c0
kdjun97 <kdjun97@gmail.com>
2024-07-17 12:54:12 +0900

    feat/student: add camera permission

--- gwasuwon/Projects/Features/HomeFeature/Sources/Student/StudentFeature.swift
+++ gwasuwon/Projects/Features/HomeFeature/Sources/Student/StudentFeature.swift
@@ -14,17 +14,22 @@ public struct StudentFeature {
 
     public struct State: Equatable {
         public init() {}
+        
+        var isCameraPermissionGranted: Bool = false
     }
 
     public enum Action {
-        case onAppear
+        case qrScanButtonTapped
+        case setCameraPermissionIsGranted(Bool)
     }
 
     public var body: some ReducerOf<StudentFeature> {
         Reduce { state, action in
             switch action {
-            case .onAppear:
+            case .qrScanButtonTapped:
                 break
+            case let .setCameraPermissionIsGranted(value):
+                state.isCameraPermissionGranted = value
             }
             return .none
         }
--- gwasuwon/Projects/Features/HomeFeature/Sources/Student/StudentView.swift
+++ gwasuwon/Projects/Features/HomeFeature/Sources/Student/StudentView.swift
@@ -7,10 +7,13 @@
 
 import SwiftUI
 import ComposableArchitecture
+import DesignSystem
+import QRScanner
 
 struct StudentView: View {
     let store: StoreOf<StudentFeature>
     @ObservedObject var viewStore: ViewStoreOf<StudentFeature>
+    @StateObject private var permissionManager = CameraPermissionManager()
 
     public init(store: StoreOf<StudentFeature>) {
         self.store = store
@@ -18,8 +21,48 @@ struct StudentView: View {
     }
     
     var body: some View {
+        StudentBodyView(viewStore: viewStore)
+        .onAppear {
+            permissionManager.requestCameraPermission()
+        }
+        .onReceive(permissionManager.$isCameraPermissionGranted) { isGranted in
+            viewStore.send(.setCameraPermissionIsGranted(isGranted))
+        }
+    }
+}
+
+private struct StudentBodyView: View {
+    private let viewStore: ViewStoreOf<StudentFeature>
+    
+    fileprivate init(viewStore: ViewStoreOf<StudentFeature>) {
+        self.viewStore = viewStore
+    }
+    
+    fileprivate var body: some View {
         VStack(spacing: 0) {
-            Text("Stutdent View")
+            GNavigationBar(title: "수업 초대받기")
+            Spacer()
+            if (viewStore.isCameraPermissionGranted) {
+                GText(
+                    "선생님의 QR 코드를 인식해주세요.",
+                    fontStyle: .Body_1_Normal_R,
+                    color: .labelAlternative
+                )
+            } else {
+                GText(
+                    "카메라 권한을 설정해주세요.",
+                    fontStyle: .Body_1_Normal_R,
+                    color: .labelAlternative
+                )
+            }
+            Spacer()
+            GButton(
+                title: "QR 인식하기",
+                style: viewStore.isCameraPermissionGranted ? .enabled : .disabled,
+                buttonAction: {
+                    viewStore.send(.qrScanButtonTapped)
+                }
+            ).hPadding(16)
         }
     }
 }
