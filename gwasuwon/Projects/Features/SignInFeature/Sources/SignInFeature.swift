da2330ba3dfe5ed42b99eb49eef728d943aff7c7
kdjun97 <kdjun97@gmail.com>
2024-07-20 00:48:01 +0900

    feat/kakao-login: connect kakao sdk to view

--- gwasuwon/Projects/Features/SignInFeature/Sources/SignInFeature.swift
+++ gwasuwon/Projects/Features/SignInFeature/Sources/SignInFeature.swift
@@ -6,35 +6,66 @@
 //
 
 import ComposableArchitecture
+import Domain
 
 public struct SignInFeature: Reducer {
+    @Dependency(\.socialUseCase) var socialUseCase
+
     public init() {}
 
     public struct State: Equatable {
         public init() {}
+        @BindingState var isLoading = false
     }
 
-    public enum Action {
+    public enum Action: BindableAction {
+        case binding(BindingAction<State>)
         case onAppear
         case kakaoButtonTapped
         case appleButtonTapped
         case navigateToHome
         case navigateToSignUp
+        case successToGetKakaoAccessToken
+        case failureToGetKakaoAccessToken
     }
 
     public var body: some ReducerOf<SignInFeature> {
+        BindingReducer()
         Reduce { state, action in
             switch action {
             case .onAppear:
                 break
+            case .binding:
+                break
             case .kakaoButtonTapped:
-                return .send(.navigateToSignUp)
+                state.isLoading = true
+                return .run { send in
+                    await send(signInWithKakao())
+                }
             case .appleButtonTapped:
                 return .send(.navigateToHome)
             case .navigateToHome, .navigateToSignUp:
                 break
+            case .successToGetKakaoAccessToken:
+                state.isLoading = false
+                break
+            case .failureToGetKakaoAccessToken:
+                state.isLoading = false
+                break
             }
             return .none
+        }
+    }
+}
+
+extension SignInFeature {
+    private func signInWithKakao() async -> Action {
+        let response = await socialUseCase.signInWithKakao()
+        switch response {
+        case let .success(result):
+            return .successToGetKakaoAccessToken
+        case let .failure(error):
+            return .failureToGetKakaoAccessToken
         }
     }
 }
